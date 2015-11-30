deae296b36537e13352193b8f718a90eb66e32bc
leahphuong <haphuong.ng@gmail.com>
2015-11-30 13:08:20 -0500

    Add register ajax on ajax.js, add eventhandler on submit button in Register form in login.js. tested, all worked!

---
+++ ajax.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var api = {
+  url: 'http://localhost:3000',
+  ajax: function(config, cb) {
+    $.ajax(config).done(function(data, textStatus, jqxhr) {
+      cb(null, data);
+    }).fail(function(jqxhr, status, error) {
+      cb({jqxher: jqxhr, status: status, error: error});
+    });
+  },
+  register: function(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/signup',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(credentials),
+      // dataType: 'json'
+    }, callback);
+  },
+};
--- login.js
+++ login.js
@@ -1,4 +1,9 @@
 $(function() {
+    $.ajaxSetup({
+       xhrFields: {
+         withCredentials: true
+    }
+    });
 
     $('#login-form-link').click(function(e) {
     $("#login-form").delay(100).fadeIn(100);
@@ -7,6 +12,35 @@ $(function() {
     $(this).addClass('active');
     e.preventDefault();
   });
+
+  var form2object = function(form) {
+    var data = {};
+    $(form).find("input").each(function(index, element) {
+      var type = $(this).attr('type');
+      if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
+        data[$(this).attr('name')] = $(this).val();
+      }
+    });
+    return data;
+  };
+
+  // {"credentials": {username: xxx, password: xxx} }
+  $('#register-form').on('submit',function(e){
+    e.preventDefault();
+    var credentials = form2object(this);
+    console.log(credentials);
+    var displayMsg = function(errors, data){
+      if(errors){
+        console.log(errors);
+      } else {
+        console.log('success');
+        console.log(data);
+      }
+    };
+    api.register(credentials, displayMsg);
+
+  });
+
   $('#register-form-link').click(function(e) {
     $("#register-form").delay(100).fadeIn(100);
     $("#login-form").fadeOut(100);
